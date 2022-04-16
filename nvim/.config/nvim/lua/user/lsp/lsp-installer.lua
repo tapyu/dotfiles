e7b150e2feef2683583af8bc738e2a9b4afc2569
tapyu <rubem.engenharia@gmail.com>
2022-04-15 22:59:42 -0300

    nvim: lsp not working

---
+++ nvim/.config/nvim/lua/user/lsp/lsp-installer.lua
@@ -0,0 +1,34 @@
+local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+if not status_ok then
+	return
+end
+
+-- Register a handler that will be called for all installed servers.
+-- Alternatively, you may also register handlers on specific server instances instead (see example below).
+lsp_installer.on_server_ready(function(server)
+	local opts = { -- these functions are applied to every single language server on the system
+		on_attach = require("user.lsp.handlers").on_attach, -- both on_attach and capabilities are functions
+		capabilities = require("user.lsp.handlers").capabilities,
+	}
+
+  -- server is the input function
+	 if server.name == "jsonls" then
+	 	local jsonls_opts = require("user.lsp.settings.jsonls")
+    -- vim is a global variable
+	 	opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+	 end
+
+	 if server.name == "sumneko_lua" then
+	 	local sumneko_opts = require("user.lsp.settings.sumneko_lua")
+	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+	 end
+
+	 if server.name == "pyright" then
+	 	local pyright_opts = require("user.lsp.settings.pyright")
+	 	opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+	 end
+
+	-- This setup() function is exactly the same as lspconfig's setup function.
+	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+	server:setup(opts)
+end)
--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -73,10 +73,16 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp" --  give us the LSP completions
+  use "hrsh7th/cmp-nvim-lua"
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+  -- LSP
+  use "neovim/nvim-lspconfig" -- enable LSP: the bare bone LPS
+  use "williamboman/nvim-lsp-installer" -- it bootstraps all of the LPS for you
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
