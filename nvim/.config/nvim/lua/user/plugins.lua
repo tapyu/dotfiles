9248d05acb50abe2c5ecdd31dafaceeff14e6e3b
tapyu <rubem.engenharia@gmail.com>
2022-04-17 13:53:48 -0300

    nvim: toggleterm - terminal integration

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -125,6 +125,9 @@ return packer.startup(function(use)
   use "akinsho/bufferline.nvim"
   use "moll/vim-bbye"
 
+  -- Toggleterm - terminal integration
+  use "akinsho/toggleterm.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
