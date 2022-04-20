432ecc03ae0a7a6f2ab3994be26dc50ef1058ec8
tapyu <rubem.engenharia@gmail.com>
2022-04-20 04:43:56 -0300

    nvim: pad-ui open at degubg event

--- nvim/.config/nvim/lua/user/dap/dap-ui.lua
+++ nvim/.config/nvim/lua/user/dap/dap-ui.lua
@@ -49,3 +49,15 @@ dap_ui.setup({
 		max_type_length = nil, -- Can be integer or nil.
 	},
 })
+
+
+local dap, dapui = require("dap"), require("dapui")
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
