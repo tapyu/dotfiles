e2a57b037a902f5ab45bcc0a3308ac05dfc80e62
tapyu <rubem.engenharia@gmail.com>
2022-05-01 15:02:47 -0300

    nvim: alph startup session

--- nvim/.config/nvim_christian/lua/user/alpha.lua
+++ nvim/.config/nvim_christian/lua/user/alpha.lua
@@ -12,9 +12,9 @@ dashboard.section.buttons.val = {
   dashboard.button("SPC s l", "  Open last session", "<cmd>SessionManager load_last_session<CR>"),
   dashboard.button("SPC f f", "  Find file", ":Telescope find_files hidden=true no_ignore=true<CR>"),
   dashboard.button("SPC f h", "  Recently opened files", "<cmd>Telescope oldfiles<CR>"),
-  dashboard.button("SPC f r", "  Frecency/MRU"),
+  -- dashboard.button("SPC f r", "  Frecency/MRU"),
   dashboard.button("SPC f g", "  Find word",  "<cmd>Telescope live_grep<cr>"),
-  dashboard.button("SPC f m", "  Jump to bookmarks"),
+  -- dashboard.button("SPC f m", "  Jump to bookmarks"),
 }
 
 alpha.setup(dashboard.config)
