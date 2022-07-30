bf8f314d65e4a7daa9205f6dddcc1b8fba8f6cf3
tapyu <rubem.engenharia@gmail.com>
2022-07-30 14:01:27 -0300

    refactor init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# PS: run it as sudo --preserve-env=HOME,SHELL sh init1.sh to preserve these variables
+# PS: run it as sudo --preserve-env=HOME,SHELL sh init.sh to preserve these variables
 # Suggesteed partitions and sizes
 # - The EFI System: -> Mount point: /boot/efi/ -> Journaling file system:efi -> size: 300MB (at least)
 # - Linux Swap -> Mount point: There isn't -> Journaling file system: linuxswap -> size: equal to memory RAM space (at moment, 24GB)
@@ -22,44 +22,44 @@ fi
 ### download and installs ###
 # Meslo patched Nerd-fonts into ~/.local/share/fonts/
 for name in {Regular,Italic,Bold-Italic}; do
-  wget --directory-prefix="$HOME/.local/share/fonts" https://raw.githubusercontents.com/ryanoasis/nerd-fonts/master/patched-fonts/Meslo/M-DZ/$name/complete/Meslo%20LG%20M%20DZ%20$(echo $name | sed -r 's/-/%20/g')%20Nerd%20Font%20Complete.ttf
+  [ ! -f "$HOME/.local/share/fonts/Meslo LG M DZ $(echo $name | sed -r 's/-/ /g') Nerd Font Complete.ttf" ] && wget --directory-prefix="$HOME/.local/share/fonts" https://raw.githubusercontents.com/ryanoasis/nerd-fonts/master/patched-fonts/Meslo/M-DZ/$name/complete/Meslo%20LG%20M%20DZ%20$(echo $name | sed -r 's/-/%20/g')%20Nerd%20Font%20Complete.ttf
 done
 
 # one-line packages
-pacman -S --nedded --noconfirm texlive-most texlive-lang texlive-bibtexextra texlive-fontsextra biber # latex files
-pacman -S --nedded --noconfirm curl # download from url
-pacman -S --nedded --noconfirm rar # rar and unrar(?) programs
-pacman -S --nedded --noconfirm github-cli # git CLI to make token password persistent
-pacman -S --nedded --noconfirm git-delta # dandavison/delta - help to improve git diff and diff commands
-pacman -S --nedded --noconfirm mlocate # a newer verion of locate command
-pacman -S --nedded --noconfirm bat # cat with syntax highlighting
-pacman -S --nedded --noconfirm lsd # the next gen ls command
-pacman -S --nedded --noconfirm telegram-desktop whatsapp-web-jak # telegram and whatsapp
-pacman -S --nedded --noconfirm bottom # a process management better than htop (call it with btm)
-pacman -S --nedded --noconfirm obs-studio # screen recoder
-pacman -S --nedded --noconfirm fzf # fuzzy finder
-pacman -S --nedded --noconfirm ripgrep # a replacment for grep
-pacman -S --nedded --noconfirm brave # web browser
-pacman -S --nedded --noconfirm vlc # video viewer
-pacman -S --nedded --noconfirm peek # .gif screen recoder
-pacman -S --nedded --noconfirm nemo # GUI file explorer
-pacman -S --nedded --noconfirm slop screenkey ttf-font-awesome # slop -> allows to select windows and/or drag over the desired region interactively without the need of calculating the coordinates manually; screenkey -> keystrokes recoder; ttf-font-awesome -> to enable nice Tux and Win icons
-pacman -S --nedded --noconfirm xclip # interface to X selections ("the clipboard") from the command line on system with an X11 implementation.
-pacman -S --nedded --noconfirm python-pip # pip command
-pacman -S --nedded frei0r-plugins breeze # kdenlive dependencies
-pacman -S --nedded audacity # audacity - audio recorder
+pacman -S --needed --noconfirm texlive-most texlive-lang texlive-bibtexextra texlive-fontsextra biber # latex files
+pacman -S --needed --noconfirm curl # download from url
+pacman -S --needed --noconfirm rar # rar and unrar(?) programs
+pacman -S --needed --noconfirm github-cli # git CLI to make token password persistent
+pacman -S --needed --noconfirm git-delta # dandavison/delta - help to improve git diff and diff commands
+pacman -S --needed --noconfirm mlocate # a newer verion of locate command
+pacman -S --needed --noconfirm bat # cat with syntax highlighting
+pacman -S --needed --noconfirm lsd # the next gen ls command
+pacman -S --needed --noconfirm telegram-desktop whatsapp-web-jak # telegram and whatsapp
+pacman -S --needed --noconfirm bottom # a process management better than htop (call it with btm)
+pacman -S --needed --noconfirm obs-studio # screen recoder
+pacman -S --needed --noconfirm fzf # fuzzy finder
+pacman -S --needed --noconfirm ripgrep # a replacment for grep
+pacman -S --needed --noconfirm brave # web browser
+pacman -S --needed --noconfirm vlc # video viewer
+pacman -S --needed --noconfirm peek # .gif screen recoder
+pacman -S --needed --noconfirm nemo # GUI file explorer
+pacman -S --needed --noconfirm slop screenkey ttf-font-awesome # slop -> allows to select windows and/or drag over the desired region interactively without the need of calculating the coordinates manually; screenkey -> keystrokes recoder; ttf-font-awesome -> to enable nice Tux and Win icons
+pacman -S --needed --noconfirm xclip # interface to X selections ("the clipboard") from the command line on system with an X11 implementation.
+pacman -S --needed --noconfirm python-pip # pip command
+pacman -S --needed frei0r-plugins breeze # kdenlive dependencies
+pacman -S --needed audacity # audacity - audio recorder
 sudo -u $SUDO_USER pip install youtube-dl # download from youtube
 sudo -u $SUDO_USER pip install trash-cli # trash-cli for KDE, GNOME, and XFCE
 # ranger and its dependencies
-pacman -S --nedded --noconfirm ranger
-pacman -S --nedded --noconfirm w3m # `w3mimgdisplay` -> for image previews
-sudo -u $SUDO_USER (cd $HOME/git/dotfiles/ && git submodule update --init --recursive) # pull ranger plugins
+pacman -S --needed --noconfirm ranger
+pacman -S --needed --noconfirm w3m # `w3mimgdisplay` -> for image previews
+(cd $HOME/git/dotfiles/ && sudo -u $SUDO_USER git submodule update --init --recursive) # pull ranger plugins
 # Rust
 sudo -u $SUDO_USER curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 sudo -u $SUDO_USER rustup override set stable
 sudo -u $SUDO_USER rustup update stable
 # alacritty
-pacman -S --nedded --noconfirm cmake freetype2 fontconfig pkg-config make libxcb libxkbcommon python # dependencies
+pacman -S --needed --noconfirm cmake freetype2 fontconfig pkg-config make libxcb libxkbcommon python # dependencies
 sudo -u $SUDO_USER cargo install alacritty
 # sublime text
 curl -O https://download.sublimetext.com/sublimehq-pub.gpg && pacman-key --add sublimehq-pub.gpg && pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
