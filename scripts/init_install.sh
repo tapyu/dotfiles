bc0918fc0574fa8da558f01efa1a4e59103895bf
tapyu <rubem.engenharia@gmail.com>
2022-08-01 17:37:51 -0300

    fix shortcut in init.sh

--- scripts/init_install.sh
+++ scripts/init_install.sh
@@ -96,6 +96,6 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings \
 # Terminal
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEY_PATH/custom0/ name "Terminal"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEY_PATH/custom0/ command "'/home/tapyu/.local/share/cargo/bin/alacritty'"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEY_PATH/custom0/  binding "Print"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEY_PATH/custom0/  binding "<Control><Alt>t"
 
 # TODO:  add keyboard
