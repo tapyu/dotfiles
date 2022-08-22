0bad8b4e3b4fb894a1d2f4956ec5278b5c96e2e2
tapyu <rubem.engenharia@gmail.com>
2022-08-22 20:14:15 -0300

    split gnome settings

--- scripts/init2_install.sh
+++ scripts/init2_install.sh
@@ -84,30 +84,3 @@ sudo -u $SUDO_USER gnome-extensions install /tmp/gtktitlebarvelitasali.github.io
 ### setting up configs and default default applications ###
 sudo -u $SUDO_USER sed -Ei 's/(^application\/pdf=).*/\1masterpdfeditor5.desktop/' ~/.config/mimeapps.list # masterpdfeditor5 to default pdf
 sudo -u $SUDO_USER gh auth login # github
-
-### GNOME settings ###
-sudo -u $SUDO_USER gsettings set org.gnome.desktop.default-applications.terminal exec /usr/local/bin/alacritty # turn alacritty the default terminal emulator
-sudo -u $SUDO_USER gsettings set org.gnome.desktop.sound allow-volume-above-100-percent 'true' # In Gnome, enable overamplification https://www.reddit.com/r/gnome/comments/exfhc4/overamplification_extension/fgbf9j2/?utm_source=share&utm_medium=web2x&context=3 
-
-### GNOME gsettings shortcut ###
-# get more ideas here: https://blog.programster.org/using-the-cli-to-set-custom-keyboard-shortcuts
-# write your solution here: https://askubuntu.com/questions/597395/how-to-set-custom-keyboard-shortcuts-from-terminal
-sudo -u $SUDO_USER KEY_PATH="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings"
-sudo -u $SUDO_USER gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings \
-"['$KEY_PATH/custom0/', '$KEY_PATH/custom1/']" # add more into this list for more shortcuts
-
-# Terminal
-sudo -u $SUDO_USER gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEY_PATH/custom0/ name "Terminal"
-sudo -u $SUDO_USER gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEY_PATH/custom0/ command "'/home/tapyu/.local/share/cargo/bin/alacritty'"
-sudo -u $SUDO_USER gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEY_PATH/custom0/  binding "<Control><Alt>t"
-
-# Web Browser
-sudo -u $SUDO_USER gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEY_PATH/custom1/ name "Brave Browser"
-sudo -u $SUDO_USER gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEY_PATH/custom1/ command "'/usr/bin/brave'"
-sudo -u $SUDO_USER gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEY_PATH/custom1/  binding "<Super>1"
-
-# TODO: add these extensions
-# https://extensions.gnome.org/extension/28/gtile/
-# https://extensions.gnome.org/extension/4496/hide-panel-light-version-without-hot-corner/
-
-# TODO:  add keyboard
