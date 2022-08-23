ba9d8e258dea8e52305bd8ad1d565ab057633d62
tapyu <rubem.engenharia@gmail.com>
2022-08-22 23:41:03 -0300

    minimal and extra arch-based distro

---
+++ scripts/arch_extra.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+### installs ###
+pacman -S --needed --noconfirm curl # download from url
+pacman -S --needed --noconfirm rar # rar and unrar(?) programs
+pacman -S --needed --noconfirm mlocate # a newer version of locate command
+pacman -S --needed --noconfirm bat # cat with syntax highlighting
+pacman -S --needed --noconfirm lsd # the next gen ls command
+pacman -S --needed --noconfirm telegram-desktop whatsapp-web-jak # telegram and whatsapp
+pacman -S --needed --noconfirm tree # see directories as tree
+pacman -S --needed --noconfirm obs-studio # screen recoder
+pacman -S --needed --noconfirm ripgrep # a replacment for grep
+pacman -S --needed --noconfirm pdfgrep # grep for pdf files
+pacman -S --needed --noconfirm vlc # video viewer
+pacman -S --needed --noconfirm gimp # image editors
+pacman -S --needed --noconfirm peek # .gif screen recoder
+pacman -S --needed --noconfirm nemo # GUI file explorer
+pacman -S --needed --noconfirm slop screenkey ttf-font-awesome # slop -> allows to select windows and/or drag over the desired region interactively without the need of calculating the coordinates manually; screenkey -> keystrokes recoder; ttf-font-awesome -> to enable nice Tux and Win icons
+pacman -S --needed --noconfirm frei0r-plugins breeze # kdenlive dependencies
+pacman -S --needed --noconfirm audacity # audacity - audio recorder
+
+sudo -u $SUDO_USER pip install youtube-dl # download from youtube
+sudo -u $SUDO_USER pip install trash-cli # trash-cli for KDE, GNOME, and XFCE
+# zoxide
+sudo -u $SUDO_USER curl -sS https://webinstall.dev/zoxide | bash
+sudo -u $SUDO_USER yay -S --needed --noconfirm insync # google drive sync
+sudo -u $SUDO_USER yay -S --needed --noconfirm kdenlive-git # kdenlive - video editor
--- scripts/gnomesettings.sh
+++ scripts/gnomesettings.sh
@@ -1,8 +1,27 @@
 #!/bin/sh
 
-### GNOME settings ###
-sudo -u $SUDO_USER gsettings set org.gnome.desktop.default-applications.terminal exec /usr/local/bin/alacritty # turn alacritty the default terminal emulator
+### GNOME default applications ###
+sudo -u $SUDO_USER gsettings set org.gnome.desktop.default-applications.terminal exec /usr/local/bin/alacritty # alacritty to default terminal emulator
+sudo -u $SUDO_USER sed -Ei 's/(^application\/pdf=).*/\1masterpdfeditor5.desktop/' ~/.config/mimeapps.list # masterpdfeditor5 to default pdf
 sudo -u $SUDO_USER gsettings set org.gnome.desktop.sound allow-volume-above-100-percent 'true' # In Gnome, enable overamplification https://www.reddit.com/r/gnome/comments/exfhc4/overamplification_extension/fgbf9j2/?utm_source=share&utm_medium=web2x&context=3 
+
+### GNOME extensions ###
+# extensions.gnome.org/extension/517/caffeine/
+# shell version: 42 extension verion: 41
+sudo -u $SUDO_USER wget -P /tmp https://extensions.gnome.org/extension-data/caffeinepatapon.info.v41.shell-extension.zip
+sudo -u $SUDO_USER gnome-extensions install /tmp/caffeinepatapon.info.v41.shell-extension.zip
+# extensions.gnome.org/extension/545/hide-top-bar/
+# shell version: 42 extension verion: 107
+sudo -u $SUDO_USER wget -P /tmp https://extensions.gnome.org/extension-data/hidetopbarmathieu.bidon.ca.v107.shell-extension.zip
+sudo -u $SUDO_USER gnome-extensions install /tmp/hidetopbarmathieu.bidon.ca.v107.shell-extension.zip
+# extensions.gnome.org/extension/615/appindicator-support/
+# shell version: 42 extension verion: 42
+sudo -u $SUDO_USER wget -P /tmp https://extensions.gnome.org/extension-data/appindicatorsupportrgcjonas.gmail.com.v42.shell-extension.zip
+sudo -u $SUDO_USER gnome-extensions install /tmp/appindicatorsupportrgcjonas.gmail.com.v42.shell-extension.zip
+# extensions.gnome.org/extension/1732/gtk-title-bar/
+# shell version: 42 extension verion: 10
+sudo -u $SUDO_USER wget -P /tmp https://extensions.gnome.org/extension-data/gtktitlebarvelitasali.github.io.v10.shell-extension.zip
+sudo -u $SUDO_USER gnome-extensions install /tmp/gtktitlebarvelitasali.github.io.v10.shell-extension.zip
 
 ### GNOME gsettings shortcut ###
 # get more ideas here: https://blog.programster.org/using-the-cli-to-set-custom-keyboard-shortcuts
@@ -10,12 +29,10 @@ sudo -u $SUDO_USER gsettings set org.gnome.desktop.sound allow-volume-above-100-
 sudo -u $SUDO_USER KEY_PATH="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings"
 sudo -u $SUDO_USER gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings \
 "['$KEY_PATH/custom0/', '$KEY_PATH/custom1/']" # add more into this list for more shortcuts
-
 # Terminal
 sudo -u $SUDO_USER gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEY_PATH/custom0/ name "Terminal"
 sudo -u $SUDO_USER gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEY_PATH/custom0/ command "'/home/tapyu/.local/share/cargo/bin/alacritty'"
 sudo -u $SUDO_USER gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEY_PATH/custom0/  binding "<Control><Alt>t"
-
 # Web Browser
 sudo -u $SUDO_USER gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEY_PATH/custom1/ name "Brave Browser"
 sudo -u $SUDO_USER gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$KEY_PATH/custom1/ command "'/usr/bin/brave'"
