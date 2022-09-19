51f129d5ee60342e4dd79b00ac0ba68ee7dfd862
tapyu <rubem.engenharia@gmail.com>
2022-09-18 22:21:02 -0300

    pass comment into youtubedl function

--- zsh/.config/zsh/zsh-user-functions.sh
+++ zsh/.config/zsh/zsh-user-functions.sh
@@ -11,14 +11,14 @@ rangercd () {
 bindkey -s '^o' 'rangercd\n'
 
 
-# function that uses youtube-dl along with ffmpeg to capture a portion of a yt video
-# $1 - youtube URL
-# $2 - start position in hh:mm:ss.msms format (ms=miliseconds)
-# $3 - final position in hh:mm:ss.msms format (ms=miliseconds)
-# $4 - output file name (optional)
-# $5 - output video codec type (optional, for instance: libx264)
-# $6 - output audio codec type (optional, for instance: aac)
 function youtubedl_snippet()(
+  # function that uses youtube-dl along with ffmpeg to capture a portion of a yt video
+  # $1 - youtube URL
+  # $2 - start position in hh:mm:ss.msms format (ms=miliseconds)
+  # $3 - final position in hh:mm:ss.msms format (ms=miliseconds)
+  # $4 - output file name (optional)
+  # $5 - output video codec type (optional, for instance: libx264)
+  # $6 - output audio codec type (optional, for instance: aac)
   local url_streams=$(youtube-dl -f best --get-url $1)
   local output_name=$(youtube-dl --get-title $1)
 
