4e364212e89612b5424dc093ebc1c8884e217416
tapyu <rubem.engenharia@gmail.com>
2023-07-20 01:05:00 -0300

    fix latexinit

--- zsh/.config/zsh/zsh-user-functions.sh
+++ zsh/.config/zsh/zsh-user-functions.sh
@@ -83,11 +83,11 @@ gp() {
 latexinit() {
 	curl --silent https://gist.githubusercontent.com/tapyu/886dc95fc19c4250fb38581ccc58bed8/raw/0eeaa62d401659fe1c57602ec8f17608775d5338/_default_preamble.tex > default_preamble.tex
   if [[ -f "${1:-main.tex}" ]]; then
-	  grep -q "\\input{default_preamble.tex}" "${1:-main.tex}" || sed -i '2i\\\input{default_preamble.tex}\n' "${1:-main.tex}"
+	  grep --quiet "\\input{default_preamble.tex}" "${1:-main.tex}" || sed -i '2i\\\input{default_preamble.tex}\n' "${1:-main.tex}"
   else
     echo -e '\\documentclass{article}\n\\input{default_preamble.tex}\n\\begin{document}\n\tHello, world!\n\\end{document}' > "${1:-main.tex}"
   fi
-	curl --silent https://gist.githubusercontent.com/tapyu/886dc95fc19c4250fb38581ccc58bed8/raw/Makefile > Makefile
+	curl --insecure --silent https://gist.githubusercontent.com/tapyu/886dc95fc19c4250fb38581ccc58bed8/raw/Makefile > Makefile
   [[ $# -ne 0 ]] && sed -i "2s/main/${1%.tex}/" Makefile
 	[[ ! -d .vscode ]] && mkdir --parents --verbose .vscode
 	curl --silent https://gist.githubusercontent.com/tapyu/886dc95fc19c4250fb38581ccc58bed8/raw/0eeaa62d401659fe1c57602ec8f17608775d5338/_vscode_makefile.json > .vscode/settings.json
