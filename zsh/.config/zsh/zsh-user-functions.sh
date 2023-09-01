b63cb276ec46d45c99d2aa7a41bc7d40879665e3
tapyu <rubem.engenharia@gmail.com>
2023-09-01 01:26:11 -0300

    delete unused functions

--- zsh/.config/zsh/zsh-user-functions.sh
+++ zsh/.config/zsh/zsh-user-functions.sh
@@ -10,30 +10,6 @@ rangercd () {
 }
 bindkey -s '^o' 'rangercd\n'
 
-
-# function that uses youtube-dl along with ffmpeg to capture a portion of a yt video
-# $1 - youtube URL
-# $2 - start position in hh:mm:ss.msms format (ms=miliseconds)
-# $3 - final position in hh:mm:ss.msms format (ms=miliseconds)
-# $4 - output file name (optional)
-# $5 - output video codec type (optional, for instance: libx264)
-# $6 - output audio codec type (optional, for instance: aac)
-youtubedl_snippet()(
-  local url_streams=$(youtube-dl -f best --get-url $1)
-  local output_name=$(youtube-dl --get-title $1)
-
-  ffmpeg -ss $2 -to $3 -i $url_streams -c:v copy -c:a copy ${4:-"$output_name.mp4"}
-)
-
-# count how many secs it takes to run a command
-stopwatch()
-{
-  local start=$SECONDS
-  command $@
-  echo "it takes $(( SECONDS - start )) seconds"
-}
-
-
 ### fzf-based commads
 fp()
 {
