a236822ff07342d9d1f2017c46a20e66041f717e
tapyu <rubem.engenharia@gmail.com>
2023-10-09 23:30:59 -0300

    switch from ranger to nnn

--- zsh/.config/zsh/zsh-user-functions.sh
+++ zsh/.config/zsh/zsh-user-functions.sh
@@ -1,14 +1,4 @@
-# Use ranger to switch directories and bind it to ctrl-o
-rangercd () {
-    tmp="$(mktemp)"
-    ranger --choosedir="$tmp" "$@"
-    if [ -f "$tmp" ]; then
-        dir="$(cat "$tmp")"
-        rm -f "$tmp"
-        [ -d "$dir" ] && [ "$dir" != "$(pwd)" ] && cd "$dir"                                               
-    fi
-}
-bindkey -s '^o' 'rangercd\n'
+bindkey -s '^o' 'nnn\n'
 
 ### fzf-based commads
 fp()
