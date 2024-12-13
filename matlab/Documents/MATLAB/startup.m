bfcc817ff04c16d0ec6596447b68e78bcc51f462
tapyu <rubem.engenharia@gmail.com>
2024-12-13 16:03:19 -0300

    update MATLAB startup.m file

--- matlab/Documents/MATLAB/startup.m
+++ matlab/Documents/MATLAB/startup.m
@@ -1,10 +1,8 @@
-% Display a custom message
-disp('Welcome to MATLAB! Custom paths have been added.');
-
 %% Add path
 if isunix
     xdg_data_home_path = getenv('XDG_DATA_HOME');
-    % if `xdg_data_home_path` is empty, the default `XDG_DATA_HOME` path
+    % if `xdg_data_home_path` is empty, fallback to the default
+    % `XDG_DATA_HOME` path
     if ~xdg_data_home_path
         xdg_data_home_path = [ '/home/' getenv('USER') '/.local/share' ];
     end
@@ -12,6 +10,7 @@ if isunix
     matlab_path = [xdg_data_home_path '/matlab'];
     if isfolder(matlab_path)
         addpath(genpath(matlab_path));
+        disp('Welcome to MATLAB! Custom paths have been added.');
     else
         error('There is no path %s to the added', matlab_path);
     end
@@ -22,3 +21,11 @@ elseif ismac
 else
     error('Unknown operating system.');
 end
+
+%% set schemer
+
+% TODO: find out how to check if the current scheme if the defaut one
+% if
+%     [dir, ~, ~] = fileparts(which('schemer_import'));
+%     schemer_import([dir '/schemes/darksteel.prf']);
+% end
