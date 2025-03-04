4b594ab2d5384c3add0de27dc54b8ab36df1e4ae
tapyu <rubem.engenharia@gmail.com>
2025-03-04 10:26:54 -0300

    incres the zsh HISTSIZE

--- zsh/.config/zsh/exports.sh
+++ zsh/.config/zsh/exports.sh
@@ -2,8 +2,8 @@
 
 # here you put the shell(local) variables
 # History in cache directory:
-HISTSIZE=10000
-SAVEHIST=10000
+HISTSIZE=150000
+SAVEHIST=150000
 HISTFILE=${XDG_STATE_HOME:-$HOME/.local/state}/zsh/history
 
 # less config
