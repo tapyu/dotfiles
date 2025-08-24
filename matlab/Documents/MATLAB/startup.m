25cd41178f0ee6ce8c8b2695d76f358824ad879f
Rubem Vasconcelos Pacelli <rubem.engenharia@gmail.com>
2025-08-24 11:03:41 -0300

    Refactor xdg_data_home_path check and clean up code
    Updated the check for xdg_data_home_path to use isempty function. Removed unused code related to setting the schemer.

--- matlab/Documents/MATLAB/startup.m
+++ matlab/Documents/MATLAB/startup.m
@@ -3,7 +3,7 @@ if isunix
     xdg_data_home_path = getenv('XDG_DATA_HOME');
     % if `xdg_data_home_path` is empty, fallback to the default
     % `XDG_DATA_HOME` path
-    if ~xdg_data_home_path
+    if isempty(xdg_data_home_path)
         xdg_data_home_path = [ '/home/' getenv('USER') '/.local/share' ];
     end
 
@@ -22,12 +22,3 @@ elseif ismac
     error('There is no default paths for macOS.');
 else
     error('Unknown operating system.');
-end
-
-%% set schemer
-
-% TODO: find out how to check if the current scheme if the defaut one
-% if
-%     [dir, ~, ~] = fileparts(which('schemer_import'));
-%     schemer_import([dir '/schemes/darksteel.prf']);
-% end
